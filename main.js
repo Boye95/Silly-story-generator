1f93da101e567d2b7f53235208aa4ae85200c532
Boye <samuel.boye95@yahoo.com>
2022-02-20 11:48:28 +0100

    Solved

--- main.js
+++ main.js
@@ -16,17 +16,31 @@ randomize.addEventListener('click', result);
 
 function result() {
 
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+  
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
+
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
-
+    const weight = Math.round(300/14) + ' stone';
+    const temperature =  Math.round((94 - 32) * 5/9) + ' centigrade';
+    newStory = newStory.replace('94vfahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
+
+
+
 }
